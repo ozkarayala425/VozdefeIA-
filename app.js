2447be061edd34446682d5cfe29d43beaf912181
VozdefeIA <ozkarayala.425@gmail.com>
2026-07-23 23:00:37 -0700

    Add function to show specific section

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+function mostrarSeccion(seccion){
 
+    document.querySelectorAll(".seccion").forEach(function(div){
+        div.style.display = "none";
+    });
+
+    document.getElementById(seccion).style.display = "block";
+}
