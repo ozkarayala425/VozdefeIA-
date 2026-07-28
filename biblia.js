6602494a235adc5a5aebc2ad8c00b0099d56291e
VozdefeIA <ozkarayala.425@gmail.com>
2026-07-27 22:51:16 -0700

    Refactor Jesús object structure in biblia.js
    Removed the old 'jesus' object and replaced it with a new 'Jesús' object that includes additional attributes.

--- biblia.js
+++ biblia.js
@@ -1,6 +1,6 @@
 const baseBiblica = {
 
-moises: {
+moises {
   nombre: "Moisés",
   palabras: [
     "moises",
@@ -19,12 +19,27 @@ moises: {
   historia: "Moisés fue llamado por Dios para sacar al pueblo de Israel de la esclavitud en Egipto. Dios habló con él desde la zarza ardiente, abrió el Mar Rojo por medio de él y le entregó los Diez Mandamientos en el monte Sinaí.",
   versiculo: "Éxodo 3:10"
 },
-
-  jesus: {
-    nombre: "Jesús",
-    historia: "Jesús es el Hijo de Dios y el Salvador del mundo. Predicó el Reino de Dios, realizó milagros, murió en la cruz por nuestros pecados y resucitó al tercer día.",
-    versiculo: "Juan 3:16"
-  },
+Jesús {
+  nombre: "Jesús",
+  palabras: [
+    "jesus",
+    "jesús",
+    "cristo",
+    "mesias",
+    "mesías",
+    "hijo de dios",
+    "nacimiento de jesus",
+    "milagros",
+    "crucifixion",
+    "crucifixión",
+    "resurreccion",
+    "resurrección",
+    "evangelios"
+  ],
+  historia: "Jesús es el Hijo de Dios y el Salvador del mundo. Predicó el Reino de Dios, realizó milagros, murió en la cruz por nuestros pecados y resucitó al tercer día.",
+  versiculo: "Juan 3:16"
+},
+  je
 
     david: {
     nombre: "David",
